ee9e203fd0be64832b9c76c3e1a3b71566dc2127
Isuri Lokupathirage <isurim2002@gmail.com>
2025-08-25 07:45:03 +0530

    my first commit

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/entity/Branch.java
@@ -0,0 +1,22 @@
+package com.cpd.hotel_system.hotel_management_service_api.entity;
+
+import com.cpd.hotel_system.hotel_management_service_api.enums.BranchType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "branch")
+public class Branch {
+    @Id
+    @Column(name = "branch_id", length = 80)
+    private String branchId;
+
+    @Column(name = "room_count")
+    private int roomCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "branch_type", nullable = false)
+    private BranchType branchType;
+
+    @Column(name = "branch_name", nullable = false)
+    private String branchName;
+}
