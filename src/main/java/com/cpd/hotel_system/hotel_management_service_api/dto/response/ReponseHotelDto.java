6e61ab221b8723be8cb86c89847ff6c37f24cd34
Isuri Lokupathirage <isurim2002@gmail.com>
2025-08-27 07:46:07 +0530

    ResponseDto completed

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/dto/response/ReponseHotelDto.java
@@ -0,0 +1,24 @@
+package com.cpd.hotel_system.hotel_management_service_api.dto.response;
+
+import com.cpd.hotel_system.hotel_management_service_api.entity.Branch;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReponseHotelDto {
+    private String hotelId;
+    private boolean activeStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String description; //even if this saved in db as blob we can use string because in website this render as html
+    private int starRating;
+    private BigDecimal startingFrom;
+    private List<ResponseBranchDto> branches;
+}
